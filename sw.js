4eff0a1d5a8f7ebc634f71174eb4e98ab61c7384
Ağa Rzayev <rzayevaga.dev@gmail.com>
2026-05-21 22:17:23 +0400

    [LM]

---
+++ sw.js
@@ -0,0 +1,30 @@
+// sw.js
+const CACHE_NAME = 'quran-life-v2';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/app.js',
+    '/data/quran.json',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    );
+});
